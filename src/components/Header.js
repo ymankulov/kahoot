e742f798d34d6a96b297995b63612e429790e5a1
BahtiyarDE <rahmnv1b@gmail.com>
2022-06-16 17:25:07 +0600

    hdsokl

--- src/components/Header.js
+++ src/components/Header.js
@@ -5,19 +5,21 @@ import Rus from '../img/rus.svg'
 
 const Header = () => {
     const header = {
-        background: "#3DBE00",
+        background: "#080077",
+        // boxShadow: "0 0 10px 5px"
+
     }
 
 
     return (
-        <div className='container'>
-            <div style={header} className='flex justify-between p-5'>
+        <div style={header} className='container'>
+            <div className=' flex justify-between pb-5 py-2'>
                 <div className='flex  justify-between flex-wrap text-white mx-20'>
                     <img src={Motion} alt=""/>
                     <h3 style={{margin: "auto"}}>MOTION <br/>TEST</h3>
                 </div>
                 <div>
-                    <button className='bg-purple-800 flex flex-wrap justify-between items-center rounded-3xl m-3 p-3  mx-20'>
+                    <button className='bg-purple-800 flex flex-wrap justify-between items-center rounded-3xl py-2 px-4 m-3 p-3  mx-20 text-white'>
                         <img src={Rus} alt=""/>
                         <h4>рус</h4>
                     </button>
@@ -28,25 +30,4 @@ const Header = () => {
 };
 
 export default Header;
-// const header = {
-//     background: "#010049",
-//     position: "fixed",
-//     width: "100%",
-//     zIndex: "999"
-// }
 
-
-// <div className='container'>
-//     <div className='bg-blue-900 flex  justify-between flex-wrap '>
-//         <div className='flex  justify-between flex-wrap'>
-//             <img src={Motion} alt=""/>
-//             <h3>MOTION <br/>TEST</h3>
-//         </div>
-//         <div>
-//             <button className='bg-purple-800 flex flex-wrap justify-between items-center rounded-full m-3 h-10'>
-//                 <img src={Rus} alt=""/>
-//                 <h4>рус</h4>
-//             </button>
-//         </div>
-//     </div>
-// </div>
--- src/components/Section.js
+++ src/components/Section.js
@@ -8,8 +8,17 @@ const Section = () => {
 
     const section = {
         background: "#001355",
-
+        // boxShadow: "0 0 10px 5px "
     }
+    const btn = {
+        width: "383px",
+        height:" 75px",
+        left: "528px",
+        top: "691px",
+    }
+
+
+
 
     return (
         <div>
@@ -18,17 +27,30 @@ const Section = () => {
                     <div className="-mt-5">
                         <img src={ellipse} alt=""/>
                     </div>
-                    <div>
+                    <div className="mt-36">
                         <img src={mac} alt=""/>
                     </div>
-                    <div className="mt-72">
+                    <div className="mt-44 ">
                         <img src={polygon} alt=""/>
                     </div>
-                </div>
 
+                </div>
+                <div className="text-center -my-16">
+                    <button style={btn}  type="button" className="focus:outline-none text-white bg-purple-700 hover:bg-purple-800
+                             focus:ring-purple-300 font-light  rounded-lg
+                            text-3xl px-10 py-4 mb-2 dark:bg-purple-600 dark:hover:bg-purple-700 dark:focus:ring-purple-900 ">Подключиться
+                    </button>
+                </div>
             </div>
         </div>
     );
 };
 
 export default Section;
+
+
+// <div className="text-center -mt-36 ">
+//     <button className="bg-purple-700 hover:bg-blue-700 text-white font-medium py-2 px-4 rounded-2xl">
+//         Подключиться
+//     </button>
+// </div>
