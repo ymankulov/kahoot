996d5860b9dac7a995b594b4f7602abd511c62bb
ymankulov <ymankulov253546@gmail.com>
2022-06-16 15:48:01 -0800

    asd

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import logo from './logo.svg';
 import './App.css';
 import Header from "./components/Header";
+import Hero from "./components/Hero";
 
 function App() {
   return (
     <div className="App">
         <Header/>
+        <Hero/>
     </div>
   );
 }
--- src/components/Header.js
+++ src/components/Header.js
@@ -1,18 +1,22 @@
 import React from 'react';
 import Motion from '../img/image 5.svg'
 import Rus from '../img/rus.svg'
+import '../css/Header.css'
+
 
 
 const Header = () => {
     return (
-        <div className='container'>
-            <div className='bg-blue-900 flex  justify-between flex-wrap '>
-                <div className='flex  justify-between flex-wrap'>
+        <div className='container header'>
+            <div className=' flex justify-between flex-wrap items-center mx-auto ' style={{
+                background: '#001355'
+            }}>
+                <div className='flex  items-center justify-between flex-wrap text-white text-2xl ml-20'>
                     <img src={Motion} alt=""/>
                     <h3>MOTION <br/>TEST</h3>
                 </div>
                 <div>
-                    <button className='bg-purple-800 flex flex-wrap justify-between items-center rounded-full m-3 h-10'>
+                    <button className=' mr-20 bg-purple-800 flex flex-wrap justify-between items-center rounded-full m-3 h-10 text-xs text-white'>
                         <img src={Rus} alt=""/>
                         <h4>рус</h4>
                     </button>
